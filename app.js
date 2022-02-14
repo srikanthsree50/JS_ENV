d08461975d2fb2284cd4d8de3827d78b01558ea2
srikanthsree50 <srikanthsree50@gmail.com>
2022-02-14 19:00:32 +0530

    github finder completed

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ ui.showAlert('User Not Found','alert alert-danger')
 }
 else{
 ui.showProfile(data.profile)
+
+ui.showRepos(data.repos)
 }
       })
     
--- github.js
+++ github.js
@@ -1,10 +1,20 @@
 class Github {
+
+constructor(){
+    this.reposCount = 5;
+    this.reposSort = 'created:asc'
+}
+
     async getUsers (user){
         const profileResponse = await fetch(`https://api.github.com/users/${user}`)
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.reposCount}&sort=${this.reposSort}`)
+     
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return {
-profile
+profile ,
+repos
         }
         
     }
