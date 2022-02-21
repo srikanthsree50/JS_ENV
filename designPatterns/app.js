6acacb1024fd7f9e8ec24d3af721d3c845e83676
srikanthsree50 <srikanthsree50@gmail.com>
2022-02-21 19:14:35 +0530

    MODULE AND REVEALING PATTERN COMMIT

---
+++ designPatterns/app.js
@@ -0,0 +1,49 @@
+
+                             ///////////             DESIGN PATTERNS  //////////
+
+
+                             ////////// MODULE PATTERN ///////
+
+// const UICntrl = (function(){
+// let text = 'hello world';
+
+// const changeText = function(){
+
+//     let element = document.querySelector('h1');
+//     element.textContent = text;
+// }
+// return{
+//     callChange: function(){
+// changeText()
+// console.log(text)
+//     }
+// }
+// })()
+
+// UICntrl.callChange();
+
+                             
+                                  //////            REVEALING MODULE PATTERNS       /////////
+
+//      const itemCntrl = (function(){
+//          let _data = [];
+
+//          function Add(item){
+// _data.push(item);
+// console.log('item added....')
+//          }
+//          function Get(id){
+//            return _data.find(item => {
+//            return    item.id === id
+//             });
+//                      }
+
+// return {
+//     add : Add,
+//     get : Get                                             ////    MAPPING SAME NAMES IS REVEALING PATTERNS
+// }
+
+//      })()                    
+     
+//      itemCntrl.add({id:561,name:'srikanth'});
+     
